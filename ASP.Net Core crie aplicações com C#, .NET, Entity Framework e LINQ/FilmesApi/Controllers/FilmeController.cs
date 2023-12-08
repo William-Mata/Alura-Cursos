837b1942bfce565c7687f8ed65d45e3e881f4aee
William Mata <50085652+William-Mata@users.noreply.github.com>
2023-12-08 18:35:11 -0300

    .NET 6: criando uma web API

---
+++ ASP.Net Core crie aplicações com C#, .NET, Entity Framework e LINQ/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : Controller
+{
+    private static List<Filme> Filmes = new List<Filme>();
+    private static int Id = 1;
+
+    [HttpPost("AdicionarFilme")] // TIPO DE REQUISIÇÃO E NOME DO METODO
+    public IActionResult AdicionarFilme([FromBody] Filme filme) //FROMBODY DEFINE QUE O CONTEUDO DA REQUISIÇÃO DEVERA VIR NO CORPO
+    {
+        filme.Id = Id++;
+        Filmes.Add(filme);
+        Console.WriteLine(filme);
+
+        return CreatedAtAction(nameof(RecuperarFilmePorId), new { id = filme.Id});
+    }
+
+    [HttpGet("ListarFilmes")] 
+    public IActionResult ListarFilmes([FromQuery] int skip, [FromQuery] int take = 50) //FROMQUERY  DEFINE QUE O CONTEUDO DA REQUISIÇÃO DEVERA VIR NA URL 
+    {
+        var filmes =  Filmes.Skip(skip).Take(take);
+
+        return Ok(filmes);
+    }
+
+    [HttpGet("RecuperarFilmePorId")] 
+    public IActionResult RecuperarFilmePorId(int id) 
+    {
+        var filme = Filmes.FirstOrDefault(x => x.Id == id)!;
+
+        if (filme == null) { return NotFound(); }
+
+        return Ok(filme);
+    }
+}
---
+++ ASP.Net Core crie aplicações com C#, .NET, Entity Framework e LINQ/FilmesApi/Models/Filme.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O titulo do filme é obrigatório.")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "A descrição do filme é obrigatória.")]
+    public string Descricao { get; set; }
+    
+    [Required]
+    [MaxLength(50, ErrorMessage = "O tamanho máximo é de 50 caracteres.")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(60.0, 240.0, ErrorMessage = "O tempo do filme deve está entre 60 a 240 minutos")]
+    public float Duracao { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}\n" +
+               $"Titulo: {Titulo}\n" +
+               $"$Descrição: {Descricao}\n" +
+               $"Genero: {Genero}\n" +
+               $"Duracao: {Duracao}\n";
+    }
+}
